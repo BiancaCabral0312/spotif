5552741fcd302261d13f98b6de2ebdb1b64d434a
Bianca Cabral <bibicabral5@gmail.com>
2026-05-10 19:23:09 -0300

    Paginas

--- src/main/java/trabalho/spotif/controller/MusicaController.java
+++ src/main/java/trabalho/spotif/controller/MusicaController.java
@@ -2,10 +2,7 @@ package trabalho.spotif.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import trabalho.spotif.model.GenerosMusicais;
 import trabalho.spotif.model.Musica;
 import trabalho.spotif.service.MusicaService;
@@ -13,30 +10,42 @@ import trabalho.spotif.service.MusicaService;
 @Controller
 public class MusicaController {
 
-    private final MusicaService musicaService = new MusicaService();
+    private final MusicaService musicaService;
+
+    public MusicaController(MusicaService musicaService) {
+        this.musicaService = musicaService;
+    }
 
     @GetMapping({"/", "/musicas"})
     public String listar(Model model) {
-        var musicas = musicaService.listarMusicas();
-        for (Musica musica : musicas) {
-            System.out.println(musica.getTitulo());
-        }
-        model.addAttribute("musicas", musicas);
+        model.addAttribute("musicas", musicaService.listarMusicas());
         model.addAttribute("generos", GenerosMusicais.values());
         model.addAttribute("musicaForm", new Musica());
-        return "musicas";
+        return "home";
+    }
+
+    @GetMapping("/musicas/adicionar")
+    public String paginaAdicionar(Model model) {
+        model.addAttribute("musicaForm", new Musica());
+        model.addAttribute("generos", GenerosMusicais.values());
+        model.addAttribute("musicas", musicaService.listarMusicas());
+        return "adicionar";
+    }
+
+    @GetMapping("/musicas/removerPagina")
+    public String paginaRemover(Model model) {
+        model.addAttribute("musicas", musicaService.listarMusicas());
+        return "remover";
     }
 
     @PostMapping("/musicas")
     public String adicionar(@ModelAttribute("musicaForm") Musica musica) {
-        Musica musicaAdicionada = musicaService.adicionarMusica(musica);
-        System.out.println("musicaAdicionada: " + musicaAdicionada.getTitulo());
+        musicaService.adicionarMusica(musica);
         return "redirect:/musicas";
     }
 
     @PostMapping("/musicas/remover")
     public String remover(@RequestParam int id) {
-        System.out.println("removendo musica com id " + id);
         musicaService.removerporId(id);
         return "redirect:/musicas";
     }
@@ -44,14 +53,13 @@ public class MusicaController {
     @PostMapping("/musicas/buscar")
     public String buscar(@RequestParam int id, Model model) {
         Musica musicaAchada = musicaService.buscarPorId(id);
-        System.out.println("musica encontrada: " + musicaAchada.getTitulo());
-        var musicas = musicaService.listarMusicas();
-        model.addAttribute("musicas", musicas);
+
+        model.addAttribute("musicas", musicaService.listarMusicas());
         model.addAttribute("generos", GenerosMusicais.values());
         model.addAttribute("musicaForm", new Musica());
         model.addAttribute("musicaBuscada", musicaAchada);
-        return "musicas";
+        model.addAttribute("buscaRealizada", true);
+
+        return "home";
     }
-
-
 }
